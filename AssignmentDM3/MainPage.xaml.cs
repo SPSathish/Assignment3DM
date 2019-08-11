c2559bf987d918b3067129a81e29373e19fbaee5
Sathish Parthasarathi <sathishsp3645@gmail.com>
2019-08-11 13:55:29 -0400

    Initial check-in of module AssignmentDM3

---
+++ AssignmentDM3/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphQL.Client;
+using GraphQL.Common.Request;
+using GraphQL.Common.Response;
+using Xamarin.Forms;
+using AssignmentDM3.Models;
+
+namespace AssignmentDM3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var client = new GraphQLClient("http://swapi.apis.guru/");
+            GraphQLRequest graphQLRequest = new GraphQLRequest
+            {
+                Query = "query{ allFilms { films {id, title, director } } }"
+            };
+            GraphQLResponse graphQLReponse = await client.PostAsync(graphQLRequest);
+            filmList.ItemsSource = graphQLReponse.Data.allFilms.films.ToObject<List<film>>();
+
+        }
+    }
+}
---
+++ AssignmentDM3/Models/film.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace AssignmentDM3.Models
+{
+    public class film
+    {
+        public string title { get; set; }
+        public string director { get; set; }
+    }
+}
